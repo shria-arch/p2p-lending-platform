7a34dff4bfadfc9368ba23687d5df16cf2139d8c
shria-arch <your.email@example.com>
2026-08-18 19:31:49 +0530

    Update investment API URL

--- frontend/src/services/investmentService.js
+++ frontend/src/services/investmentService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://16.16.34.145:3002/api/investments";
+const API_URL = "http://13.51.194.116:3002/api/investments";
 
 export const createInvestment = async (loanId, amountFunded) => {
   const token = localStorage.getItem("token");
